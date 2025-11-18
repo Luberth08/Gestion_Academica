f7efe34323b78c153d675b4795552170dca5b170
Luberth08 <luberthgutierrez@gmail.com>
2025-11-18 00:32:03 -0400

    nube 5

--- BACK-END/app/Http/Controllers/usuario/PermisoController.php
+++ BACK-END/app/Http/Controllers/usuario/PermisoController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-use App\Services\bitacoraService;
+use App\Services\BitacoraService;
 
 class PermisoController extends Controller
 {
--- BACK-END/app/Http/Controllers/usuario/authController.php
+++ BACK-END/app/Http/Controllers/usuario/authController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
-use App\Services\bitacoraService;
+use App\Services\BitacoraService;
 
 class authController extends Controller
 {
